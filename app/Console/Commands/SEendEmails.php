925802c2a0b8d3a681aa15c0c23f07f2bcf3835a
Hamza Khan <hamzi3307@gmail.com>
2023-08-05 12:42:00 +0500

    Initial Commit

---
+++ app/Console/Commands/SEendEmails.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SEendEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:s-eend-emails';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
