6a8c9bde6e58dda5e367e98fbcb6752ae1c75f87
Hamza Khan <hamzi3307@gmail.com>
2023-08-05 13:04:30 +0500

    Update travellist.blade.php

--- resources/views/travellist.blade.php
+++ resources/views/travellist.blade.php
@@ -5,14 +5,14 @@
 
 <body>
 	<h1>My Travel Bucket List</h1>
-	<h2>Places I'd Like to Visit</h2>
+	<h2>Places I'd Like to Visit:</h2>
 	<ul>
 	  @foreach ($togo as $newplace)
 		<li>{{ $newplace->Name }}</li>
 	  @endforeach
 	</ul>
 
-	<h2>Places I've Already Been To</h2>
+	<h2>Places I've Already Been To:</h2>
 	<ul>
           @foreach ($visited as $place)
                 <li>{{ $place->Name }}</li>
